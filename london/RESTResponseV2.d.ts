06f7059ad1bcae811f6323b285a1649a41de25f1
John Caruso <johncaruso@email.com>
2019-03-13 18:08:00 -0500

    Add partial JSDoc fo RESTResponseV2

--- london/RESTResponseV2.d.ts
+++ london/RESTResponseV2.d.ts
@@ -1,13 +1,52 @@
-interface RESTResponseV2 {
-  getBody(): string;
-  getCookies(): { size: () => number; get: (index: number) => string };
-  getErrorCode(): number;
-  getErrorMessage(): string;
-  getHeader(name: string): string;
-  getHeaders(): object;
-  getQueryString(): string;
-  getResponseAttachmentSysid(): string;
-  getStatusCode(): number;
-  haveError(): boolean;
-  waitForResponse(timeoutSecs: number): void;
+declare namespace sn_ws {
+  /**
+   * The RESTResponseV2 API allows you to use the data returned by an outbound REST message
+   * in JavaScript code.
+   * @class RESTResponseV2
+   * @typedef {Object}  RESTResponseV2
+   */
+  declare class RESTResponseV2 {
+    /**
+     * Instantiates a RESTMessageV2 object using information from a REST message record.
+     *
+     * You must have a REST message record defined before you can use this constructor.
+     *
+     * In the following example, replace `REST_message_record` with the name of the REST message
+     * record from your instance.
+     *
+     * @example
+     *
+     *
+     */
+    constructor();
+
+    /**
+     * Returns all headers contained in the response, including any duplicate
+     * headers.
+     * @returns The list of headers contained in the response. Each header is represented as a
+     * GlideHTTPHeader object which contains the header name and
+     * value.
+     * @example
+     *
+     * var r = new sn_ws.RESTMessageV2('<A REST message>', 'get');
+     * var response = r.execute();
+     * var headers = response.getAllHeaders();
+     * for(var i in headers){
+     * gs.print(headers[i].name + ': ' + headers[i].value);
+     * }
+     */
+    getAllHeaders(): [{ name: string; value: string }];
+
+    getBody(): string;
+    getCookies(): { size: () => number; get: (index: number) => string };
+    getErrorCode(): number;
+    getErrorMessage(): string;
+    getHeader(name: string): string;
+    getHeaders(): object;
+    getQueryString(): string;
+    getResponseAttachmentSysid(): string;
+    getStatusCode(): number;
+    haveError(): boolean;
+    waitForResponse(timeoutSecs: number): void;
+  }
 }
--- london/_index.js
+++ london/_index.js
@@ -1,24 +1,6 @@
-// @ts-nocheck
-// ts-nocheck
-var current, template, email;
-
-if (current.approver.getTableName() == 'sysapproval_approver') {
-  if (current.approver == email.from_sys_id) {
-    current.comments = 'reply from: ' + email.from + '\n\n' + email.body_text;
-    // if it's been cancelled, it's cancelled.
-    var doit = true;
-    if (current.state == 'cancelled') doit = false;
-    if (email.body.state != undefined) current.state = email.body.state;
-    if (doit) current.update();
-  } else {
-    gs.log(
-      'Approval for task (' +
-        current.sysapproval.getDisplayValue() +
-        ') rejected because user sendingemail( ' +
-        email.from +
-        ') does not match the approver (' +
-        current.approver.getDisplayValue() +
-        ')'
-    );
-  }
+var r = new sn_ws.RESTMessageV2('x_318721_symbol_sp.get_incident', 'get');
+var response = r.execute();
+var headers = response.getAllHeaders();
+for (var i in headers) {
+  gs.info(headers[i].name + ': ' + headers[i].value);
 }
