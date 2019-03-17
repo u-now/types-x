80f0a7287ca9bdb34abfea382f7eaf6c329c86d8
John Caruso <johncaruso@email.com>
2019-03-17 16:00:05 -0500

    Prep for DefinitelyTyped PR

--- london/_index.js
+++
@@ -1,6 +0,0 @@
-var r = new sn_ws.RESTMessageV2('x_318721_symbol_sp.get_incident', 'get');
-var response = r.execute();
-var headers = response.getAllHeaders();
-for (var i in headers) {
-  gs.info(headers[i].name + ': ' + headers[i].value);
-}
